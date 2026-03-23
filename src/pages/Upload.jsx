378b3da431719f3117b53ff817cc4d65f8961bd9
Gaurav Bhardwaj <gauravbhardwaj7489@gmail.com>
2026-03-23 16:22:00 +0530

    update upload API URL to use environment variable

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -10,7 +10,7 @@ export default function Upload() {
     const formData = new FormData();
     formData.append("file", file);
 
-    const res = await fetch("http://127.0.0.1:8000/upload", {
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/upload`, {
       method: "POST",
       body: formData,
     });
